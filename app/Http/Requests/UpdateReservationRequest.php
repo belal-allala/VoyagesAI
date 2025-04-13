97884f417b17b1e9a80b0babb0d60426f12ec5dd
belal-allala <belalallala810@gmail.com>
2025-04-13 22:42:09 +0100

    Define validation rules in UpdateReservationRequest (reusing CreateReservationRequest rules)

--- app/Http/Requests/UpdateReservationRequest.php
+++ app/Http/Requests/UpdateReservationRequest.php
@@ -11,18 +11,23 @@ class UpdateReservationRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
      */
     public function rules(): array
     {
         return [
-            //
-        ];
+            'bus_id' => 'required|exists:buses,id',
+            'passenger_name' => 'required|string|max:255',
+            'passenger_email' => 'required|email|max:255',
+            'seat_count' => 'required|integer|min:1|max:10',
+            'total_price' => 'required|numeric|min:0',
+            'reservation_date' => 'required|date_format:Y-m-d H:i:s',
+        ]; 
     }
 }
