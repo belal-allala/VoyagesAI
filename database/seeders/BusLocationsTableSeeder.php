27fb278081ead8a412ea8c05742de90c00e13304
belal-allala <belalallala810@gmail.com>
2025-04-13 20:29:01 +0100

    Implement BusLocations table seeder with simulated data

--- database/seeders/BusLocationsTableSeeder.php
+++ database/seeders/BusLocationsTableSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Bus;
+use App\Models\BusLocation;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
 
 class BusLocationsTableSeeder extends Seeder
 {
@@ -12,6 +15,15 @@ class BusLocationsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $buses = Bus::all();
+
+        foreach ($buses as $bus) {
+            BusLocation::create([
+                'bus_id' => $bus->id,
+                'latitude' => fake()->latitude(45, 50),
+                'longitude' => fake()->longitude(2, 7),
+                'timestamp' => Carbon::now()->subMinutes(rand(0, 60)),
+            ]);
+        }
     }
 }
