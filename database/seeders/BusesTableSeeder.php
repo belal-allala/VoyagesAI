093f627a8e0e3e756d4710d63ec4b145969c03e4
belal-allala <belalallala810@gmail.com>
2025-04-13 19:24:21 +0100

    Implement Buses table seeder with test data

--- database/seeders/BusesTableSeeder.php
+++ database/seeders/BusesTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Bus;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,24 @@ class BusesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Bus::create([
+            'departure_city' => 'Paris',
+            'destination_city' => 'Lyon',
+            'departure_time' => '2024-09-15 08:00:00',
+            'price' => 25.00,
+            'capacity' => 50,
+            'available_seats' => 30,
+            'company_name' => 'Bus Company A',
+        ]);
+
+        Bus::create([
+            'departure_city' => 'Marseille',
+            'destination_city' => 'Bordeaux',
+            'departure_time' => '2024-09-16 14:30:00',
+            'price' => 30.50,
+            'capacity' => 45,
+            'available_seats' => 25,
+            'company_name' => 'Bus Company B',
+        ]);
     }
 }
