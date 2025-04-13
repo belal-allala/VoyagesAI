b9ef399af27fcbc1df0f27dc66b7916fc7e92837
belal-allala <belalallala810@gmail.com>
2025-04-13 20:28:05 +0100

    Create BusLocations table seeder

---
+++ database/seeders/BusLocationsTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BusLocationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
