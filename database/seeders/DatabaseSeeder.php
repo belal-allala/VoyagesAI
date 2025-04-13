aa73d04cc3f59b6b24b2228a393ba7b5a0837f41
belal-allala <belalallala810@gmail.com>
2025-04-13 21:39:54 +0100

    Run Reservations table seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             BusesTableSeeder::class,
             BusLocationsTableSeeder::class,
+            ReservationsTableSeeder::class,
         ]);
     }
 }
