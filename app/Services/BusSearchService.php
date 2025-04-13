b65ff0e61f8bab24a72ce066c553928201ddb031
belal-allala <belalallala810@gmail.com>
2025-04-13 19:39:31 +0100

    Create BusSearchService

---
+++ app/Services/BusSearchService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+use App\Models\Bus;
+
+class BusSearchService
+{
+    public function searchBuses(array $filters)
+    {
+        $query = Bus::query();
+
+        if (isset($filters['destination_city'])) {
+            $query->where('destination_city', $filters['destination_city']);
+        }
+
+        return $query->get(); 
+    }
+}
