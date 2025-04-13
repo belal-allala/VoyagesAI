de59051a825d39ffa394f558c03bc09fc90220fb
belal-allala <belalallala810@gmail.com>
2025-04-13 19:22:49 +0100

    Configure fillable attributes in Bus model

--- app/Models/Bus.php
+++ app/Models/Bus.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Bus extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'departure_city',
+        'destination_city',
+        'departure_time',
+        'price',
+        'capacity',
+        'available_seats',
+        'company_name',
+    ];
 }
