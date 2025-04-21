bf7159b0568561500e79f9204c94f0e1bd287ee0
belal-allala <belalallala810@gmail.com>
2025-04-21 11:48:44 +0100

    Implement user login logic in AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User; 
 use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Support\Facades\Hash; 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -32,5 +34,27 @@ class AuthController extends Controller
             'access_token' => $token,
             'token_type' => 'Bearer',
         ], 201);
+    }
+
+    /**
+     * Login user and create a new token.
+     *
+     * @param  \App\Http\Requests\LoginRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(LoginRequest $request) 
+    {
+        if (!Auth::attempt($request->validated())) { 
+            return response()->json(['message' => 'Identifiants invalides.'], 401); 
+        }
+
+        $user = Auth::user(); 
+        $token = $user->createToken('auth_token')->plainTextToken; 
+
+        return response()->json([
+            'user' => $user,
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ], 200); 
     }
 }
