ace58302d17f99c75033284e02859574f93524ff
belal-allala <belalallala810@gmail.com>
2025-04-21 15:06:42 +0100

    Implement profile retrieval logic in ProfileController

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Resources\UserResource; // Importez UserResource
 
 class ProfileController extends Controller
 {
-    //
+    /**
+     * Get user profile.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function profile(Request $request) 
+    {
+        $user = $request->user(); 
+
+        return new UserResource($user); 
+    }
 }
