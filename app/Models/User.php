f5394892de774c4a709688bf2d839e77122aa995
belal-allala <belalallala810@gmail.com>
2025-04-21 12:13:59 +0100

    Add hasRole method to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -42,4 +43,15 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Check if the user has a specific role.
+     *
+     * @param  string  $role
+     * @return bool
+     */
+    public function hasRole(string $role): bool // <-- Méthode hasRole
+    {
+        return $this->role === $role;
+    }
 }
