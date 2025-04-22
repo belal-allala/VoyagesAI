452ca1798d06213cd659a9097d2ca15d8ae9b310
belal-allala <belalallala810@gmail.com>
2025-04-22 10:27:22 +0100

    Modify ReservationConfirmation constructor to receive Reservation model

--- app/Notifications/ReservationConfirmation.php
+++ app/Notifications/ReservationConfirmation.php
@@ -6,46 +6,62 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use App\Models\Reservation; // Importez le modèle Reservation
 
 class ReservationConfirmation extends Notification
 {
     use Queueable;
 
     /**
-     * Create a new notification instance.
+     * The reservation instance.
+     *
+     * @var \App\Models\Reservation
      */
-    public function __construct()
+    protected $reservation; 
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param  \App\Models\Reservation  $reservation 
+     * @return void
+     */
+    public function __construct(Reservation $reservation)
     {
-        //
+        $this->reservation = $reservation; 
     }
 
     /**
      * Get the notification's delivery channels.
      *
-     * @return array<int, string>
+     * @param  mixed  $notifiable
+     * @return array
      */
-    public function via(object $notifiable): array
+    public function via($notifiable)
     {
         return ['mail'];
     }
 
     /**
      * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
      */
-    public function toMail(object $notifiable): MailMessage
+    public function toMail($notifiable)
     {
         return (new MailMessage)
                     ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
+                    ->action('View Notification', url('/'))
                     ->line('Thank you for using our application!');
     }
 
     /**
      * Get the array representation of the notification.
      *
-     * @return array<string, mixed>
+     * @param  mixed  $notifiable
+     * @return array
      */
-    public function toArray(object $notifiable): array
+    public function toArray($notifiable)
     {
         return [
             //
