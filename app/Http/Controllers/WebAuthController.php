635e994f3fe347abd82a74ca87be7b1502ef9565
belal-allala <belalallala810@gmail.com>
2025-04-23 15:14:26 +0100

    Move web registration logic from AuthController to WebAuthController

--- app/Http/Controllers/WebAuthController.php
+++ app/Http/Controllers/WebAuthController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 
 class WebAuthController extends Controller
 {
-    //
+    /**
+     * Register a new user.
+     *
+     * @param  \App\Http\Requests\RegisterRequest  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function register(RegisterRequest $request) 
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role,
+        ]);
+
+        return redirect()->route('home')->with('message', 'Compte créé avec succès. Veuillez vous connecter.');
+    }
 }
