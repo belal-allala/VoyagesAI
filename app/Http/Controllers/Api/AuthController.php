747ed062b7d39c36bb610976fcb2b47d893cb3c9
belal-allala <belalallala810@gmail.com>
2025-04-23 15:17:36 +0100

    Suppressed register methods for better architecture api/web

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\User; 
+use App\Models\User;
 use App\Http\Requests\RegisterRequest;
 use App\Http\Requests\LoginRequest;
-use Illuminate\Support\Facades\Hash; 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
@@ -18,44 +18,44 @@ class AuthController extends Controller
      * @param  \App\Http\Requests\RegisterRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function register(RegisterRequest $request)
+   /*  public function register(RegisterRequest $request)
     {
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->password), 
+            'password' => Hash::make($request->password),
             'role' => $request->role,
         ]);
 
-        $token = $user->createToken('auth_token')->plainTextToken; 
+        $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json([
             'user' => $user,
             'access_token' => $token,
             'token_type' => 'Bearer',
         ], 201);
-    }
+    }*/
 
-    /**
+     /**
      * Login user and create a new token.
      *
      * @param  \App\Http\Requests\LoginRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function login(LoginRequest $request) 
+    public function login(LoginRequest $request)
     {
-        if (!Auth::attempt($request->validated())) { 
-            return response()->json(['message' => 'Identifiants invalides.'], 401); 
+        if (!Auth::attempt($request->validated())) {
+            return response()->json(['message' => 'Identifiants invalides.'], 401);
         }
 
-        $user = Auth::user(); 
-        $token = $user->createToken('auth_token')->plainTextToken; 
+        $user = Auth::user();
+        $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json([
             'user' => $user,
             'access_token' => $token,
             'token_type' => 'Bearer',
-        ], 200); 
+        ], 200);
     }
 
     /**
@@ -66,7 +66,8 @@ class AuthController extends Controller
      */
     public function logout(Request $request)
     {
-        $request->user()->currentAccessToken()->delete(); 
+        $request->user()->currentAccessToken()->delete();
+
         return response()->json(['message' => 'Déconnexion réussie.']);
     }
 }
