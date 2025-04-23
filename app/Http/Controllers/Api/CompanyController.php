8b330c611863460da1414b257bae57853aabdeea
belal-allala <belalallala810@gmail.com>
2025-04-23 10:13:47 +0100

    Implement company deletion logic in CompanyController

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -53,8 +53,10 @@ class CompanyController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Company $company) 
     {
-        //
+        $company->delete();
+
+        return response()->noContent(204);
     }
 }
