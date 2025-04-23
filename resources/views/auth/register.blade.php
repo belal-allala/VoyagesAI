24140a567059060a37027b9b31781ed40b7af54c
belal-allala <belalallala810@gmail.com>
2025-04-23 15:12:49 +0100

    Create WebAuthController for web authentication

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -36,7 +36,6 @@
             <label for="role" class="block text-gray-700 text-sm font-bold mb-2">Rôle:</label>
             <select id="role" name="role" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
                 <option value="voyageur" selected>Voyageur</option>
-                <!-- Option pour les compagnies à ajouter plus tard (avec contrôle d'accès) -->
             </select>
             @error('role') <p class="text-red-500 text-xs italic">{{ $message }}</p> @enderror
         </div>
